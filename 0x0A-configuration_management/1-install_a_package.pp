c13a8839e49f53e2d976d367a684de5769b71f2a
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-02-23 23:45:26 +0200

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,6 @@
-# Ensure Python 3 is installed
-package { 'python3': ensure => installed }
-
 #Using Puppet, install flask from pip3
 
-package { 'flask':
-ensure   => '2.1.0',
-provider => 'pip3'
+package { 'Flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
