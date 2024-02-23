67ce690b38408dd4f47383c2f678896db3e538db
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-02-23 23:52:08 +0200

    0x0A-configuration_management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-#Using Puppet, install flask from pip3
+# install puppet-lint -v 2.5.0
 
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'puppet-lint':
+  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
 }
