33833331fa498bea85a1d4bd6ba227f9a901386d
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-02-29 03:00:30 +0200

    0x0C-web_server

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -4,7 +4,7 @@ package { 'nginx':
 provider => 'apt',
 }
 exec {'hlbtn_page':
-command => '/usr/bin/sudo /bin/echo Holberton School > /var/www/html/index.nginx-debian.html',
+command => '/usr/bin/sudo /bin/echo 'Hello World!' > /var/www/html/index.nginx-debian.html',
 }
 exec {'redirect_page':
 
