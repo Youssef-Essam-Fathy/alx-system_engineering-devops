00f563846146cf1a7266e0ba54d1905387e89cbd
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-03-26 02:37:09 +0200

    0x15-api

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''importing requests module'''
+import requests
+import sys
+
+if __name__ == "__main__":
+    employeeId = sys.argv[1]
+    employee = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                            .format(employeeId))
+    name = employee.json().get("name")
+
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    totalTasks = 0
+    completed = 0
+
+    for task in todos.json():
+        if task.get('userId') == int(employeeId):
+            totalTasks += 1
+            if task.get('completed'):
+                completed += 1
+
+    print('Employee {} is done with tasks({}/{}):'
+          .format(name, completed, totalTasks))
+
+    print('\n'.join(["\t" + task.get('title') for task in todos.json()
+          if task.get('userId') == int(employeeId) and task.get('completed')]))
