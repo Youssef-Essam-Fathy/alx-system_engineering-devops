c31840fbe449f6f85746e2975fd96b6cbe253845
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-04-12 03:39:05 +0200

    0x17-web_stack_debugging_3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,6 @@
-# Fixes bad 'phpp'
-exec {
-    command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php'
-    path => '/usr/local/bin/:/bin/'
+# Creat a manifest that fix all termintion of phpp.
+
+exec { 'fix_phpp':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => ['/bin', '/usr/bin/', '/usr/loca/bin/']
 }
