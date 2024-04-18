caacfc112ed4fa52aac98a6618d0c337bc734bd4
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-04-18 07:00:28 +0200

    0x1B-web_stack_debugging_4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,18 @@
 #Change the OS configuration so that it is possible to login with the holberton user and open a file without any error message.
 
-exec { 'change-os-configuration-for-holberton-user':
-  command => 'ulimit -n 4096',
-  path   => '/usr/local/bin/:/bin/
+file { '/etc/security/limits.conf':
+  content => "holberton hard nofile 97816\nholberton soft nofile 97816\n",
+  owner   => 'root',
+  group   => 'root',
+  mode    => '0644',
+}
+
+exec { 'reload_pam':
+  command     => '/bin/pkill -HUP -u holberton bash',
+  refreshonly => true,
+  subscribe   => File['/etc/security/limits.conf'],
+}
+
+user { 'holberton':
+  ensure => present,
 }
