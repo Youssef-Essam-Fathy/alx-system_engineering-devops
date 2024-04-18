e9c45ac95666d4d91ed89641722e5f27de1908b9
Youssef-Essam-Fathy <youssefessam5623@gmail.com>
2024-04-18 06:54:30 +0200

    0x1B-web_stack_debugging_4

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,5 @@
+#Change the OS configuration so that it is possible to login with the holberton user and open a file without any error message.
+
+exec { 'change-os-configuration-for-holberton-user':
+  command => 'ulimit -n 4096',
+}
